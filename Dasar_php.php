dda113f74ee686593fbccd0684c0c9fab72e2430
fita hidayah <fitahidayah260601@gmail.com>
2022-10-30 19:30:06 +0700

    memassukan kode dasar php

---
+++ Dasar_php.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Fita Hidayah";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Ahmad Alfan Ahsani";
+$umurAdik = 24;
+
+echo "Nama kakak saya <strong>$namaAdik</strong>, kakak saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur-$umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
